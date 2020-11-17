1b3b51f3b472d82a992fa3fedc470a0e8b85690a
geb <853934146@qq.com>
2020-11-17 16:47:35 +0800

    add naive bayes classifier

--- tests/test_bolt_nlp.py
+++ tests/test_bolt_nlp.py
@@ -1,5 +1,7 @@
 import pytest
 from pybolt.bolt_nlp import WordDiscover
+from pybolt.bolt_nlp.naive_bayes_classification.utils import get_data_set_test
+from pybolt.bolt_nlp import BernoulliClassifier
 
 
 class TestBoltNlp(object):
@@ -7,6 +9,13 @@ class TestBoltNlp(object):
     def test_word_discover(self):
         wd = WordDiscover()
         wd.word_discover(["/home/geb/PycharmProjects/pybolt/tests/data/examples.txt"])
+
+    def test_bernoulli_nb(self):
+        inputs, targets = get_data_set_test([("./data/adv.cut", 'Ad'),
+                                             ("./data/not_adv.cut", 'Normal')])
+        bc = BernoulliClassifier()
+        bc.fit(inputs, targets)
+        assert round(bc.accuracy, 16) == 0.9959251101321586
 
 
 if __name__ == "__main__":
