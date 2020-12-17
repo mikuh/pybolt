9171ac62adb535418672e4dc24aafca6a4e9a674
geb <853934146@qq.com>
2020-12-17 15:23:22 +0800

    update

--- tests/test_bolt_nlp.py
+++ tests/test_bolt_nlp.py
@@ -6,21 +6,21 @@ from pybolt.bolt_nlp import Tokenizer
 
 class TestBoltNlp(object):
 
-    def test_word_discover(self):
-        wd = WordDiscover()
-        wd.word_discover(["/home/geb/PycharmProjects/pybolt/tests/data/examples.txt"])
-
-    def test_bernoulli_nb(self):
-        inputs, targets = get_data_set_test([("./data/adv.cut", 'Ad'),
-                                             ("./data/not_adv.cut", 'Normal')])
-        bc = BernoulliClassifier()
-        bc.fit(inputs, targets)
-        assert round(bc.accuracy, 16) == 0.9959251101321586
+    # def test_word_discover(self):
+    #     wd = WordDiscover()
+    #     wd.word_discover(["/home/geb/PycharmProjects/pybolt/tests/data/examples.txt"])
+    #
+    # def test_bernoulli_nb(self):
+    #     inputs, targets = get_data_set_test([("./data/adv.cut", 'Ad'),
+    #                                          ("./data/not_adv.cut", 'Normal')])
+    #     bc = BernoulliClassifier()
+    #     bc.fit(inputs, targets)
+    #     assert round(bc.accuracy, 16) == 0.9959251101321586
 
     def test_tokenizer(self):
-        tk = Tokenizer("/home/geb/PycharmProjects/nlp-data/default_dict.txt")
+        tk = Tokenizer("/home/geb/PycharmProjects/nlp-data/taptap_dict.txt")
         assert " ".join(tk.cut("你刺客攻击没我萨满高hhh,可以加\/吗")) == "你 刺客 攻击 没 我 萨满 高 hhh , 可以 加\/ 吗"
-
+        print(" ".join(tk.cut("不知道为什么评价里会有人污蔑我操纵评论")))
 
 if __name__ == "__main__":
     pytest.main(["-s", "test_blot_nlp.py"])
