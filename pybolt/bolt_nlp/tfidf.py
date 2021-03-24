303600e1f3bd07d10ae7e3416780df8508af2973
geb <853934146@qq.com>
2021-03-24 10:27:04 +0800

    update tfidf logger

--- pybolt/bolt_nlp/tfidf.py
+++ pybolt/bolt_nlp/tfidf.py
@@ -3,6 +3,7 @@ from collections import Counter
 import numpy as np
 from pybolt.utils import default_logger as logging
 import joblib
+import time
 
 
 class TFIDF(object):
@@ -21,9 +22,11 @@ class TFIDF(object):
         logging.info("Init idf successfully!")
 
     def load_idf(self, file: str):
+        start = time.time()
         idf = joblib.load(file)
         self.total_doc_num = idf["total_doc_num"]
         self.word_doc_count = idf["word_doc_count"]
+        logging.info("Loading idf cost {} seconds.".format(time.time() - start))
 
     def save_idf(self, file: str):
         if self.total_doc_num == 0:
@@ -50,7 +53,7 @@ class TFIDF(object):
         length = len(sentence)
         if length == 0:
             return []
-        return [self.tfidf(word, word_count[word]/length) for word in sentence]
+        return [self.tfidf(word, word_count[word] / length) for word in sentence]
 
 
 if __name__ == '__main__':
@@ -64,7 +67,8 @@ if __name__ == '__main__':
 
     print(tfidf.idf("This"))
     print(tfidf.idf("haha"), tfidf.tfidf("haha", 0.2))
-    print(tfidf.tfidf_sentece("This is the first document."))  # [0.13862943611198905, 0.05753641449035617, 0.0, 0.13862943611198905, 0.13862943611198905]
+    print(tfidf.tfidf_sentece(
+        "This is the first document."))  # [0.13862943611198905, 0.05753641449035617, 0.0, 0.13862943611198905, 0.13862943611198905]
 
     tfidf.save_idf("text.idf")
 
